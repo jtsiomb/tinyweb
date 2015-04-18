a2cf24de157af3c5c57de76845d443c0bff43144
John Tsiombikas <nuclear@member.fsf.org>
2015-04-18 23:55:34 +0300

    comments in tinyweb.h and README file

---
+++ README.rst
@@ -0,0 +1,22 @@
+tinyweb - tiny web server library and daemon
+============================================
+
+The tinyweb project includes a rudimentary web server in library form
+(libtinyweb), and a simple web server daemon (tinywebd) which uses it.
+
+Author: John Tsiombikas <nuclear@member.fsf.org>
+
+I disclaim all copyright to this program, and place it in the public domain.
+Feel free to use it any way you like. I'd appreciate a mention and an email
+saying hi, but whatever.
+
+Download
+--------
+Latest release: no releases yet, just grab the source from the repo.
+
+Code repository: https://github.com/jtsiomb/tinyweb
+
+Usage
+-----
+See ``src/main.c`` as an example on how to use libtinyweb, and read the header
+file ``libtinyweb/src/tinyweb.h`` which is very simple, and heavily commented.
--- libtinyweb/src/tinyweb.h
+++ libtinyweb/src/tinyweb.h
@@ -8,8 +8,24 @@ int tw_set_logfile(const char *fname);
 int tw_start(void);
 int tw_stop(void);
 
+/* tw_get_sockets returns the number of active sockets managed by tinyweb
+ * (clients plus the listening socket), and fills in the array sockets
+ * passed through the socks pointer, if it's not null.
+ *
+ * Call it with a null pointer initially to get the number of sockets, then
+ * make sure you have enough space in the array and pass it in a second call
+ * to fill it.
+ */
 int tw_get_sockets(int *socks);
+
+/* returns the maximum file descriptor number in the set of sockets managed
+ * by the library (useful for calling select).
+ */
 int tw_get_maxfd(void);
+
+/* call tw_handle_socket to let tinyweb handle incoming traffic to any of the
+ * tinyweb managed sockets.
+ */
 int tw_handle_socket(int s);
 
 
